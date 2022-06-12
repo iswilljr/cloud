08eabdd83abfe7db29c26b5f0d1b0ac3baa34052
iswilljr <iswilljr@gmail.com>
2022-06-12 17:22:50 -0500

    run test without tsc on pre-commit

--- cloud-api/src/middlewares/err.ts
+++ cloud-api/src/middlewares/err.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
 export const err = (err: any, req: Request, res: Response, next: NextFunction) => {
-	console.log("ee");
 	res.status(err.statusCode || 404).json({ success: false, message: err.message });
 };
