64325a87671b54100b40ee0a39ec371e2173a90e
iswilljr <iswilljr@gmail.com>
2022-06-12 15:07:46 -0500

    [+] Test

--- cloud-api/blob.test.js
+++ cloud-api/blob.test.js
@@ -9,16 +9,10 @@ describe("Blob endpoint", () => {
 	it("should read test file", async () => {
 		const res = await request(app).get(`/blob${TEST_PATH}`);
 		expect(res.statusCode).toEqual(200);
-		expect(res.body).toHaveProperty("success");
-		expect(res.body).toHaveProperty("info");
-		expect(res.body).toHaveProperty("content");
-		expect(res.body).toHaveProperty("path");
 		expect(res.body.success).toEqual(true);
 		expect(res.body.path).toEqual(TEST_PATH);
-		expect(res.body.info.isDirectory).toEqual(false);
-		expect(res.body.info.isFile).toEqual(true);
 		expect(res.body.info.isFile).toEqual(true);
 		expect(res.body.content.type).toEqual("file");
-		expect(res.body.content.data).toEqual("ok");
+		expect(res.body.content.data).toEqual("ok\n");
 	});
 });
--- cloud-api/list.test.js
+++ cloud-api/list.test.js
@@ -5,27 +5,17 @@ describe("List endpoint", () => {
 	it("should list parent root directory", async () => {
 		const res = await request(app).get("/ls");
 		expect(res.statusCode).toEqual(200);
-		expect(res.body).toHaveProperty("success");
-		expect(res.body).toHaveProperty("info");
-		expect(res.body).toHaveProperty("content");
-		expect(res.body).toHaveProperty("path");
 		expect(res.body.success).toEqual(true);
 		expect(res.body.path).toEqual("/");
 		expect(res.body.info.isDirectory).toEqual(true);
-		expect(res.body.info.isFile).toEqual(false);
 	});
 	it("should read test directory", async () => {
 		const dirname = "__cloud_api_test__";
 		const res = await request(app).get(`/ls/${dirname}`);
 		expect(res.statusCode).toEqual(200);
-		expect(res.body).toHaveProperty("success");
-		expect(res.body).toHaveProperty("info");
-		expect(res.body).toHaveProperty("content");
-		expect(res.body).toHaveProperty("path");
 		expect(res.body.success).toEqual(true);
 		expect(res.body.path).toEqual(`/${dirname}`);
 		expect(res.body.info.isDirectory).toEqual(true);
-		expect(res.body.info.isFile).toEqual(false);
 		expect(res.body.content.type).toEqual("directory");
 		expect(res.body.content.data.files).toHaveLength(1);
 		expect(res.body.content.data.directories).toHaveLength(0);
