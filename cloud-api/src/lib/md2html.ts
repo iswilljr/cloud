56dc02c3e02f70b286d5db38efee99d85df9b29e
iswilljr <iswilljr@gmail.com>
2022-06-21 19:40:09 -0500

    updated md2html error log

--- cloud-api/src/lib/md2html.ts
+++ cloud-api/src/lib/md2html.ts
@@ -12,8 +12,10 @@ async function md2html(markdown: string) {
 			headers,
 		}).then((r) => r.text());
 		return html.replace(/\n$/, "");
-	} catch (error) {
+	} catch (error: any) {
 		console.log("[-] Something went wrong with md2html, make sure your token is correct");
+		console.log(`[-] Token: ${token?.replace(/_.*/g, (str) => "_" + "*".repeat(str.length))}`);
+		console.log(`[-] Error: ${error.message}`);
 		return false;
 	}
 }
