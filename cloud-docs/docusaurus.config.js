b95ecf33a4f42a92bee5ad469d2aa2fb3d32c5e2
iswilljr <iswilljr@gmail.com>
2022-07-04 20:15:03 -0500

    Updated projectName from cloud-docs

--- cloud-docs/docusaurus.config.js
+++ cloud-docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
 	onBrokenMarkdownLinks: "warn",
 	favicon: "img/favicon.ico",
 	organizationName: "iswilljr",
-	projectName: "cloud/cloud-docs",
+	projectName: "cloud",
 	trailingSlash: false,
 	i18n: { defaultLocale: "en", locales: ["en"] },
 	presets: [
