e1057eb77b3a43cc09961f593f2c06c22ee482c1
iswilljr <iswilljr@gmail.com>
2022-07-05 20:20:22 -0500

    V2.0.4

---
+++ cloud-setup/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const shelljs = require("shelljs");
+
+const repo = process.argv[2];
+
+if (!repo | !["api", "deno", "github"].includes(repo)) {
+	console.log(repo);
+	process.exit(1);
+}
+
+const repoPath = path.join(process.cwd(), `cloud-${repo}`);
+const script = `
+mkdir ${repoPath}
+cd ${repoPath}
+curl https://codeload.github.com/iswilljr/cloud/tar.gz/master | \
+tar -xz --strip=2 cloud-master/cloud-${repo}
+`;
+
+shelljs.exec(script);
