e94cc3a26a5258f711c4dadc28c022576c329250
iswilljr <iswilljr@gmail.com>
2022-07-08 17:32:26 -0500

    added sync-branches script

---
+++ scripts/sync-branches.sh
@@ -0,0 +1,27 @@
+git checkout master
+git pull
+
+git checkout cloud-api
+git pull
+git merge master
+git push
+
+git checkout cloud-deno
+git pull
+git merge master
+git push
+
+git checkout cloud-github
+git pull
+git merge master
+git push
+
+git checkout cloud-setup
+git pull
+git merge master
+git push
+
+# git checkout create-home-cloud
+# git pull
+# git merge master
+# git push
