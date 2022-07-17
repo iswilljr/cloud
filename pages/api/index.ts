56ad42191e1d31ef53c398d8a7cab6ace7d1d98a
Will <97823389+iswilljr@users.noreply.github.com>
2022-07-16 19:59:53 -0500

    Delete pages/api/index.ts

--- pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import os from "os";
-import fs from "fs/promises";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function Haandler(_: NextApiRequest, res: NextApiResponse) {
-  return res.json(await fs.readdir(os.homedir() + "/dev"));
-}
